fc042010fd66378f87ccf407c572e3df2d31f854
Ishwar <ishwara.ttl@tatamotors.com>
2023-03-13 15:59:48 +0530

    scheduler2 code updated

--- schedule2.py
+++ schedule2.py
@@ -1,6 +1,21 @@
+import os
+import datetime
 
+from django.core.wsgi import get_wsgi_application
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
+application = get_wsgi_application()
 
+from get_crm.models import CRM_Data_Staging, Max_date_Table, EolCertClientTelematics
+# three_year_ago = datetime.datetime.now() - datetime.timedelta(days=3*365)
 
-CRM_Data_Staging.objects.filter(data_received_date=)
+datetime_last_schedule  = Max_date_Table.objects.order_by('id').first().datetime
+
+obj_staging = CRM_Data_Staging.objects.filter(data_received_date__gt=datetime_last_schedule).values()
+
+print(obj_staging)
+
+# for i in obj_staging:
+#     i['staging_id'] = i.pop('id')
+#     EolCertClientTelematics.objects.filter().update(**i)
