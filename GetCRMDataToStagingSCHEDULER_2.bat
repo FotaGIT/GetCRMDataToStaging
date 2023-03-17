21ab20ebf92b3b0d8810d0cf144e1f6925347369
Ishwar <ishwara.ttl@tatamotors.com>
2023-03-17 14:15:03 +0530

    bat file added

---
+++ GetCRMDataToStagingSCHEDULER_2.bat
@@ -0,0 +1,2 @@
+"D:\Omkar_Share\kafka-ssl\kafka_python\Scripts\python.exe" "D:\ishwar\GetCRMDataToStaging\schedule2.py"
+@REM "D:\VJ Share\ishwar\GetCRMDataToStaging\venv\Scripts\python.exe" "D:\VJ Share\ishwar\GetCRMDataToStaging\schedule.py"
